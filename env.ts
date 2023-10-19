9da0c82b8ff2b499a9f8f53a10cb077a3e7c6856
Sonny <sonnyasdev@gmail.com>
2023-10-19 09:58:59 +0200

    fix: update env.ts

--- env.ts
+++ env.ts
@@ -27,5 +27,5 @@ export default Env.rules({
   MYSQL_PORT: Env.schema.number(),
   MYSQL_PASSWORD: Env.schema.string(),
   MYSQL_ROOT_PASSWORD: Env.schema.string(),
-  DB_NAME: Env.schema.string(),
+  MYSQL_DB_NAME: Env.schema.string(),
 });
