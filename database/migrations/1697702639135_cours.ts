da4d210549dab7aeae442f05b0fb2feff44122b9
Sonny <sonnyasdev@gmail.com>
2023-10-19 10:18:20 +0200

    feat: add cours model/migration

---
+++ database/migrations/1697702639135_cours.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
+
+export default class extends BaseSchema {
+  protected tableName = 'cours';
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id');
+      table.string('titre', 100).unique().notNullable();
+      table.string('description').notNullable();
+      table.string('enseignant', 50).notNullable();
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true });
+      table.timestamp('updated_at', { useTz: true });
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
