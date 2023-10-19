7bc1472e5694bfe9994b0e5c4c91fed9d98c9076
Thomas PEREZ <92369255+ThomasPerez91@users.noreply.github.com>
2023-10-19 11:39:00 +0200

    feat: add registrations (#9)
    Co-authored-by: Diall Thydus <pro.thomas.perez@gmail.com>

--- database/migrations/1697707112326_registrations.ts
+++ database/migrations/1697707112326_registrations.ts
@@ -1,14 +1,14 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'notes'
+  protected tableName = 'registrations'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('student_id').unsigned().references('id').inTable('students').onDelete('CASCADE').notNullable()
       table.integer('cours_id').unsigned().references('id').inTable('cours').onDelete('CASCADE').notNullable()
-      table.float('notes', 2).notNullable()
+      table.date('date_inscription').notNullable()
 
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
