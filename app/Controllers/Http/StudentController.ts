98dfe698ffc8fdc0c3ac6b4607331547f7b29701
ThomasPerez91 <pro.thomas.perez@gmail.com>
2023-10-22 00:17:58 +0200

    chore: add related datas on queries

--- app/Controllers/Http/StudentController.ts
+++ app/Controllers/Http/StudentController.ts
@@ -1,20 +1,40 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Student from 'App/Models/Student';
 import StudentValidator from 'App/Validators/StudentValidator';
 
-
-
 export default class StudentController {
+  // @no-swagger
+  public async showStudent({ request, inertia }: HttpContextContract) {
+    const student = await this.getStudentById(request.param('studentId'));
+    return inertia.render('Students/ShowStudent', { student });
+  }
+
+  // @no-swagger
+  public async showCreate({ inertia }: HttpContextContract) {
+    return inertia.render('Students/Create');
+  }
+
+  // @no-swagger
+  public async showEdit({ request, inertia }: HttpContextContract) {
+    const student = await this.getStudentById(request.param('studentId'));
+    return inertia.render('Students/Edit', { student });
+  }
+
+  // @no-swagger
+  public async showDelete({ request, inertia }: HttpContextContract) {
+    const student = await Student.findOrFail(request.param('studentId'));
+    return inertia.render('Students/Delete', { student });
+  }
 
   public async index({ response }: HttpContextContract) {
-    const students = await Student.all();
+    const students = await Student.query().preload('courses');
     return response.json({ students });
   }
 
-    public async getStudent({ response, request }: HttpContextContract) {
-        const students = await Student.findBy('id', request.param('studentId'));
-        return response.json({ students });
-    }
+  public async getStudent({ response, request }: HttpContextContract) {
+    const students = await Student.findBy('id', request.param('studentId'));
+    return response.json({ students });
+  }
 
   public async createStudent({ response, request }: HttpContextContract) {
     const data = await request.validate(StudentValidator);
@@ -42,5 +62,11 @@ export default class StudentController {
     const course = await Student.findOrFail(studentId);
     await course.delete();
     return response.json({ message: 'Student deleted', course });
+  }
+
+  private async getStudentById(studentId: number) {
+    const student = await Student.findOrFail(studentId);
+    await student.load('courses');
+    return student;
   }
 }
